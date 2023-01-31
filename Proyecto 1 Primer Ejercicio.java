6eaff23d247c8e124c60a5cfb59cccb3d3786fff
IanAlexander21 <123991098+IanAlexander21@users.noreply.github.com>
2023-01-31 02:00:52 -0300

    Add files via upload

---
+++ Proyecto 1 Primer Ejercicio.java
@@ -0,0 +1,11 @@
+public class MyClass {
+    public static void main(String args[]) {
+        int resultado = 0;
+        resultado = suma(50, 100, 30);
+        System.out.println(resultado);
+    }
+    
+    public static int suma(int a, int b, int c){
+        return a + b + c;
+    }
+}
