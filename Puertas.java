2cca679884c9ba63025268a9e7e6ce9dca303989
IanAlexander21 <123991098+IanAlexander21@users.noreply.github.com>
2023-01-31 03:23:56 -0300

    Add files via upload

---
+++ Puertas.java
@@ -0,0 +1,21 @@
+public class Puertas{
+    
+    public static void main(String[] args){
+        Coche miCoche = new Coche();
+        miCoche.AgregarPuerta();
+        
+        System.out.println(miCoche.puertas);
+    }  
+
+}
+    
+    class Coche {
+        public int puertas = 4;
+        
+        public void AgregarPuerta(){
+            this.puertas++;
+        }
+        
+    }
+    
+    
